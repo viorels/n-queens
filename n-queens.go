7a05eb560f413db06de8f7d6e5f9f2dd4711034a
Viorel Stirbu <viorels@gmail.com>
2017-05-05 17:14:15 +0100

    go fmt

--- n-queens.go
+++ n-queens.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 /*
@@ -12,48 +12,48 @@ column = index
 line = board[index]
 */
 
-func try_queen(n int, board []int) (int) {
-    count := 0
-    for line := 0; line < n; line++ {
-        board_new_queen := append(board, line)
-        if verify(board_new_queen) {
-            if len(board_new_queen) == n {
-                count++
-            } else {
-                count += try_queen(n, board_new_queen)
-            }
-        }
-    }
-    return count
+func try_queen(n int, board []int) int {
+	count := 0
+	for line := 0; line < n; line++ {
+		board_new_queen := append(board, line)
+		if verify(board_new_queen) {
+			if len(board_new_queen) == n {
+				count++
+			} else {
+				count += try_queen(n, board_new_queen)
+			}
+		}
+	}
+	return count
 }
 
-func verify(board []int) (bool) {
-    last_col := len(board) - 1
-    last_line := board[last_col]
-    for col, line := range board[:last_col] {
-        if line == last_line || abs(last_line - line) == abs(last_col - col) {
-            return false
-        }
-    }
-    return true
+func verify(board []int) bool {
+	last_col := len(board) - 1
+	last_line := board[last_col]
+	for col, line := range board[:last_col] {
+		if line == last_line || abs(last_line-line) == abs(last_col-col) {
+			return false
+		}
+	}
+	return true
 }
 
-func abs(i int) (int) {
-    if i < 0 {
-        return -i
-    } else {
-        return i
-    }
+func abs(i int) int {
+	if i < 0 {
+		return -i
+	} else {
+		return i
+	}
 }
 
 func main() {
-    n := 8
-    if len(os.Args) > 1 {
-        if i, err := strconv.Atoi(os.Args[1]); err == nil {
-            n = i
-        }
-    }
-    board := make([]int, 0, n)
-    count := try_queen(n, board)
-    fmt.Println(count)
+	n := 8
+	if len(os.Args) > 1 {
+		if i, err := strconv.Atoi(os.Args[1]); err == nil {
+			n = i
+		}
+	}
+	board := make([]int, 0, n)
+	count := try_queen(n, board)
+	fmt.Println(count)
 }
